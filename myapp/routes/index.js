fd64dc869989cdc0fd79c5d2654558cbbf364142
Sourav.pathak@fitcircle.in <SouravPathak@Administrators-MacBook-Air.local>
2021-11-18 17:32:44 +0530

    leaderboard

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,52 +1,26 @@
 var express = require('express');
-const { MongoClient } = require('mongodb');
+var rewardService  = require('../services/rewardService');
 
-const uri = "mongodb+srv://newuser:newpassword@ozivarun.0yawr.mongodb.net/ozivarundb?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
 
-async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    // Establish and verify connection
-    
-    console.log("Connected successfully to server");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/incomingrewards', async function(req,res,next)
+{  
+rewardService.updateRewards('arpit','1234567890',10);
+
 });
+
 
 router.get('/leaderboard', async function(req, res, next) {
 
-  client.connect(
-    async err=>
-    {
-      let resp  = client.db("ozivarundb").collection("userinfo").find().limit(10).sort( {coin : -1} );
-      console.log(await resp.count());
-      let leadersList = [];
-      await resp.forEach(element => {
-        leadersList.push(element);
-        console.log(element);
-      });
-      console.log(leadersList);
-
+  
+  let leadersList = await rewardService.getLeaderboard();
       res.render('leaderboard', { title: 'oZiva virtual run leaderboard' ,leaders:leadersList});
 
     }
   );
-
-  
-});
-
 
 module.exports = router;
---
+++ myapp/services/rewardService.js
@@ -0,0 +1,51 @@
+const { MongoClient } = require('mongodb');
+
+
+const uri = "mongodb+srv://newuser:newpassword@ozivarun.0yawr.mongodb.net/ozivarundb?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+const rewardService = {
+    updateRewards : async function(phone,name,coins)
+    {
+        client.connect(
+            async err=>
+            {
+            let userInfo = await client.db("ozivarundb").collection("userinfo").findOne({'phone':phone});
+
+            console.log(userInfo);
+
+            if(userInfo)
+            {
+                await client.db("ozivarundb").collection("userinfo").updateOne(userInfo,
+                {
+                    $set:{count:Number(Number(userInfo.coins)+coins)}
+                });
+            }
+            else 
+            {
+                await client.db("ozivarundb").collection("userinfo").insertOne({
+                    'name':name,'phone':phone,'coins':Number(coins)
+                });
+            }
+        } );
+    },
+    getLeaderboard : async function() {
+        let leadersList = [];
+        client.connect(
+            async err=>
+            {
+              let resp  = client.db("ozivarundb").collection("userinfo").find().limit(10).sort( {coin : -1} );
+              console.log(await resp.count());
+              
+              await resp.forEach(element => {
+                leadersList.push(element);
+                console.log(element);
+              });
+              console.log(leadersList);
+            }
+        );
+      return leadersList;
+    }
+  };
+
+module.exports = rewardService;
