9f536f5b5ff5a6694c6d93189e4fdae2da30022c
Sourav.pathak@fitcircle.in <SouravPathak@Administrators-MacBook-Air.local>
2021-11-18 15:30:37 +0530

    mongodb connection

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,4 +1,24 @@
 var express = require('express');
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb+srv://newuser:newpassword@ozivarun.0yawr.mongodb.net/ozivarundb?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 var router = express.Router();
 
 /* GET home page. */
@@ -6,8 +26,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/leaderboard', function(req, res, next) {
-  res.render('leaderboard', { title: 'oZiva virtual run leaderboard' });
+router.get('/leaderboard', async function(req, res, next) {
+
+  client.connect(
+    async err=>
+    {
+      let resp  = client.db("ozivarundb").collection("userinfo").find().limit(10).sort( {coin : -1} );
+      console.log(await resp.count());
+      let leaders = [];
+      await   resp.forEach(element => {
+        leaders.push(element);
+        console.log(element);
+      });
+      console.log(leaders);
+
+      res.render('leaderboard', { title: 'oZiva virtual run leaderboard' ,leaders});
+
+    }
+  );
+
+  
 });
 
 
